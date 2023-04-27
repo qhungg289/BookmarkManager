21463f8bcfa478397859b95c8a3c439d901b05c6
Hung Duong Quang <qhungg289@protonmail.com>
2023-04-27 10:40:28 +0700

    Add DbSet properties

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BookmarkManager.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookmarkManager.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Bookmark> Bookmarks { get; } 
+        public DbSet<Folder> Folders { get; }
+        public DbSet<Tag> Tags { get; }
     }
 }
