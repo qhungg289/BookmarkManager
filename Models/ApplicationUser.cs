100fc30949b8bbeaa4a5a4776cc698cea3ec56cc
Hung Duong Quang <qhungg289@protonmail.com>
2023-04-27 10:40:53 +0700

    Add ApplicationUser model

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BookmarkManager.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public List<Bookmark> Bookmarks { get; set; } = new List<Bookmark>();
+        public List<Folder> Folders { get; set; } = new List<Folder>();
+    }
+}
