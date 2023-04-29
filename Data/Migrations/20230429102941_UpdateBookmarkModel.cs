2f8b48c710467941253479a28a459130763befe4
Hung Duong Quang <qhungg289@protonmail.com>
2023-04-29 21:27:51 +0700

    cập nhật migration

---
+++ Data/Migrations/20230429102941_UpdateBookmarkModel.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookmarkManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBookmarkModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookmarkTag_Tag_TagsId",
+                table: "BookmarkTag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tag",
+                table: "Tag");
+
+            migrationBuilder.RenameTable(
+                name: "Tag",
+                newName: "Tags");
+
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Bookmarks",
+                newName: "Icon");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tags",
+                table: "Tags",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookmarkTag_Tags_TagsId",
+                table: "BookmarkTag",
+                column: "TagsId",
+                principalTable: "Tags",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookmarkTag_Tags_TagsId",
+                table: "BookmarkTag");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tags",
+                table: "Tags");
+
+            migrationBuilder.RenameTable(
+                name: "Tags",
+                newName: "Tag");
+
+            migrationBuilder.RenameColumn(
+                name: "Icon",
+                table: "Bookmarks",
+                newName: "Image");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tag",
+                table: "Tag",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BookmarkTag_Tag_TagsId",
+                table: "BookmarkTag",
+                column: "TagsId",
+                principalTable: "Tag",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
